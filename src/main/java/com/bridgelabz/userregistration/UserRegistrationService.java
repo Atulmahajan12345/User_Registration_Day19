d97237b6bd19191a225dcd489264d49f04d12623
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-04-12 16:51:20 +0530

    check passwords has a special characters

--- src/main/java/com/bridgelabz/userregistration/UserRegistrationService.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrationService.java
@@ -4,7 +4,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationService
 {
-	
+	/**
+	 * @param method to check first name 
+	 */
 	public void checkFirstName(String firstName) 
 	{
 		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(firstName).matches();
@@ -18,7 +20,9 @@ public class UserRegistrationService
 		}
 	}
 
-	
+	/**
+	 * @param method to check last name 
+	 */
 	public void checkLastName(String lastName) 
 	{
 		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(lastName).matches();
@@ -32,7 +36,9 @@ public class UserRegistrationService
 		}
 	}
 
-	
+	/**
+	 * @param method to check email address 
+	 */
 	public void checkEmailAddress(String emailAddress)
 	{
 		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+([.,+,_,-]{1}[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2})?").matcher(emailAddress).matches();
@@ -45,7 +51,9 @@ public class UserRegistrationService
 			System.out.println("Email is invalid");
 		}
 	}
-	
+	/**
+	 * @param method to check phone number 
+	 */
 	public void checkPhoneNUmber(String phoneNumber) 
 	{
 		boolean IsMatched = Pattern.compile("^[0-9]{2}[ ][0-9]{10}$").matcher(phoneNumber).matches();
@@ -60,10 +68,12 @@ public class UserRegistrationService
 
 	}
 
-	
+	/**
+	 * @param method to check password 
+	 */
 	public void checkPassword(String password)
 	{
-		boolean IsMatched = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}").matcher(password).matches();
+		boolean IsMatched = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=[^@#!$%^&+=]*$)(?=.*[0-9]).{8,}").matcher(password).matches();
 		if(IsMatched)
 		{
 			System.out.println("Password is correct");

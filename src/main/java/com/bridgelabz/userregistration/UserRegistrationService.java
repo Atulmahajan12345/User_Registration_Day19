1ffcde02d12c50546dc1b6a53647cad26b7926fd
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-04-12 15:59:15 +0530

    method to check password should have 8 characters

--- src/main/java/com/bridgelabz/userregistration/UserRegistrationService.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrationService.java
@@ -2,10 +2,9 @@ package com.bridgelabz.userregistration;
 
 import java.util.regex.Pattern;
 
-public class UserRegistrationService {
-	/**
-	 * @param method to check first name 
-	 */
+public class UserRegistrationService
+{
+	
 	public void checkFirstName(String firstName) 
 	{
 		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(firstName).matches();
@@ -18,10 +17,7 @@ public class UserRegistrationService {
 			System.out.println("First name is invalid");
 		}
 	}
-	
-	/**
-	 * @param method to check last name 
-	 */
+
 	public void checkLastName(String lastName) 
 	{
 		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(lastName).matches();
@@ -34,10 +30,8 @@ public class UserRegistrationService {
 			System.out.println("Larst name is invalid");
 		}
 	}
+
 	
-	/**
-	 * @param method to check email address 
-	 */
 	public void checkEmailAddress(String emailAddress)
 	{
 		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+([.,+,_,-]{1}[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2})?").matcher(emailAddress).matches();
@@ -50,9 +44,7 @@ public class UserRegistrationService {
 			System.out.println("Email is invalid");
 		}
 	}
-	/**
-	 * @param method to check phone number 
-	 */
+	
 	public void checkPhoneNUmber(String phoneNumber) 
 	{
 		boolean IsMatched = Pattern.compile("^[0-9]{2}[ ][0-9]{10}$").matcher(phoneNumber).matches();
@@ -64,6 +56,20 @@ public class UserRegistrationService {
 		{
 			System.out.println("Phone number is invalid");
 		}
-		
+
+	}
+
+	
+	public void checkPassword(String password)
+	{
+		boolean IsMatched = Pattern.compile("[A-Za-z0-9]{8,}").matcher(password).matches();
+		if(IsMatched)
+		{
+			System.out.println("Password is correct");
+		}
+		else 
+		{
+			System.out.println("Password is invalid");
+		}
 	}
 }
